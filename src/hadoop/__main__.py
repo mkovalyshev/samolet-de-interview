a46f6fa481984656c647806cb8934ae9381c0eb0
Mikhail Kovalyshev <makovalyshev@edu.hse.ru>
2022-09-11 20:41:39 +0300

    hadoop refactor

--- src/hadoop/__main__.py
+++ src/hadoop/__main__.py
@@ -1,38 +1,37 @@
+from dataclasses import dataclass
 import os
+import argparse
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
 
 HADOOP_HOST = "localhost"
 HADOOP_PORT = "9000"
 
-RELATIVE_FILEPATH = "parser/data"
-FILENAME = "output_1662830659.csv"
+HADOOP_PATH = "user/kovalyshev/samolet"
+HADOOP_TABLE = "smartphones.parquet"
 
-FILEPATH = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__))), RELATIVE_FILEPATH
-)
+RELATIVE_FILEPATH = "parser/data"
 
 POSTGRES_USER = "samolet"
-POSTGRES_PASSWORD = "1234"
+POSTGRES_PASSWORD = "1111"
 POSTGRES_HOST = "localhost"
 POSTGRES_PORT = 5432
 POSTGRES_DATABASE = "postgres"
 POSTGRES_DRIVER = "/Users/kovalyshev/postgresql-42.5.0.jar"
 
+POSTGRES_TABLE = "smartphones"
+
 # did not set env variables for reproducibility
 
 
-def extract(spark: object, path: str = FILEPATH, filename: str = FILENAME) -> object:
+def extract(spark: object, path: str, filename: str) -> object:
     """
     reads local .csv from path/filename, returns instance of pyspark DataFrame
     """
 
     path = os.path.join(path, filename)
 
-    data = spark.read \
-        .format("csv") \
-        .option("header", "true") \
-        .load(f"file://{path}")
+    data = spark.read.format("csv").option("header", "true").load(f"file://{path}")
 
     return data
 
@@ -65,45 +64,49 @@ def transform(raw_data: object) -> object:
 
 def to_hadoop(
     data: object,
+    path: str,
+    table: str,
     host: str = HADOOP_HOST,
     port: int = HADOOP_PORT,
-    path: str = FILEPATH,
-    filename: str = FILENAME,
 ) -> None:
     """
     loads pyspark DataFrame to HDFS (located at host:port) as .parquet
     """
 
-    data.write \
-        .mode("overwrite") \
-        .option("header", "true") \
-        .parquet(f"hdfs://{host}:{port}/{path}/{filename}")
+    data.write.mode("overwrite").option("header", "true").parquet(
+        f"hdfs://{host}:{port}/{path}/{table}"
+    )
 
 
 def to_database(
-    data: object, 
+    data: object,
     table: str,
-    host: str = POSTGRES_HOST, 
+    host: str = POSTGRES_HOST,  # too many arguments, would change to DBConnection class
     port: int = POSTGRES_PORT,
-    database: str = POSTGRES_DATABASE, 
+    database: str = POSTGRES_DATABASE,
     user: str = POSTGRES_USER,
-    password: str = POSTGRES_PASSWORD
+    password: str = POSTGRES_PASSWORD,
 ) -> None:
     """
     writes data to RDB
     """
 
-    data.write.format("jdbc") \
-        .option("url", f"jdbc:postgresql://{host}:{port}/{database}") \
-        .option("driver", "org.postgresql.Driver") \
-        .option("dbtable", table) \
-        .option("user", user) \
-        .option("password", password) \
-        .mode("overwrite") \
-        .save()
+    data.write.format("jdbc").option(
+        "url", f"jdbc:postgresql://{host}:{port}/{database}"
+    ).option("driver", "org.postgresql.Driver").option("dbtable", table).option(
+        "user", user
+    ).option(
+        "password", password
+    ).mode(
+        "overwrite"
+    ).save()
 
 
 if __name__ == "__main__":
+
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("-f", dest="f", type=str, required=True)
+    cli_args = arg_parser.parse_args()
 
     spark = (
         SparkSession.builder.master("local")
@@ -112,12 +115,18 @@ if __name__ == "__main__":
         .getOrCreate()
     )
 
-    raw_data = extract(spark, FILEPATH, FILENAME)
+    filename = cli_args.f
+
+    filepath = os.path.join(
+        os.path.dirname(os.path.dirname(os.path.abspath(__file__))), RELATIVE_FILEPATH
+    )
+
+    raw_data = extract(spark, filepath, filename)
 
     data = transform(raw_data)
 
-    to_hadoop(data, path="user/kovalyshev/samolet", filename="smartphones.parquet")
+    to_hadoop(data, path=HADOOP_PATH, table=HADOOP_TABLE)
 
-    to_database(data, "smartphones")
+    to_database(data, POSTGRES_TABLE)
 
     spark.stop()

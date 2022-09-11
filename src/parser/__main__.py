975497aaad063c20b85e42ca49b0bb869efa3c2a
Mikhail Kovalyshev <makovalyshev@edu.hse.ru>
2022-09-11 20:26:23 +0300

    refactored parser

--- src/parser/__main__.py
+++ src/parser/__main__.py
@@ -1,53 +1,53 @@
-from dataclasses import dataclass
-import argparse
-import time
-import requests
 import re
 import os
 import csv  # because pandas seems to much for this
+import sys
+import time
+import logging
+from typing import Union
+from dataclasses import dataclass
+import argparse
+import requests
 from bs4 import BeautifulSoup
 
-SCHEMA = "https"
-HOST = "alfa.kz"
-PHONES_PATH = "phones/telefony-i-smartfony"
-# for this task it can be hardcoded, for production version it should be an element of category tree
+
+CALL_MESSAGE = "{} returned {}"
+
+OUTPUT_PATH = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    f"data/output_{int(time.time())}.csv",
+)
+
+logger = logging.getLogger(__file__)
+logger.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(stream=sys.stdout)
+handler.setFormatter(logging.Formatter("[%(asctime)s: %(levelname)s] %(message)s"))
+logger.addHandler(handler)
 
 
-class Parser:
-    def __init__(self, schema: str = SCHEMA, host: str = HOST, path: str = PHONES_PATH):
-        self.schema = schema
-        self.host = host
-        self.path = path
+@dataclass
+class Log:
+    level: int
+    msg: str
 
-    def get_products_from_page(self, page: int):  # name too verbose?
-        """
-        returns all product URLs from page defined in parameter page
-        """
 
-        response = requests.get(
-            f"{self.schema}://{self.host}/{self.path}/page{page}#products"
-        )
+def log(func: callable):
+    def wrapper(*args, **kwargs):
+        try:
+            level = logging.INFO
+            func_return = func(*args, **kwargs)
+        except BaseException as e:
+            level = logging.WARNING
+            func_return = e
 
-        if response.status_code != 200:
-            raise Exception(
-                f"""
-            request error on Parser.get_products_from_page();
-            status_code = {response.status_code};
-            """
-            )
+        log = Log(level, CALL_MESSAGE.format(func.__qualname__, func_return))
+        logger.log(**log.__dict__)
 
-        soup = BeautifulSoup(
-            response.text, features="html.parser"
-        )  # full regex parser would probably increase perf, bs decreases dev time
+        if not isinstance(func_return, BaseException):
+            return func_return
 
-        products = soup.find_all("div", {"itemtype": "http://schema.org/Product"})
-
-        links = [
-            product.find("div", {"class": "title"}).find("a").get("href")
-            for product in products
-        ]
-
-        return links
+    return wrapper
 
 
 @dataclass
@@ -58,27 +58,25 @@ class Product:
     ram: int
     memory: int
 
-    rom_pattern = re.compile(", Встроенная память: (\d+)(\D+),")
-    ram_pattern = re.compile(", Оперативная память: (\d+)(\D+),")
-    # group is needed for memory unit, would add in production version
+    _rom_pattern = re.compile(", Встроенная память: (\d+)(\D+),")
+    _ram_pattern = re.compile(", Оперативная память: (\d+ \D+),")
+    _itemtype = "http://schema.org/Product"
 
     @staticmethod
-    def from_link(link: str) -> object:
+    @log
+    def from_link(url: str) -> object:
         """
         returns instance of Product from link
         """
 
-        response = requests.get(link)
+        response = requests.get(url)
 
         if response.status_code != 200:
-            raise Exception(
-                f"""
-            request error on Product.from_link();
-            status_code = {response.status_code};
-            """
+            raise requests.exceptions.HTTPError(
+                f"status_code = {response.status_code}, url = {url}"
             )
 
-        soup = BeautifulSoup(response.text, features="html.parser")
+        soup = BeautifulSoup(response.text, features=Parser.PARSER)
 
         title = soup.find("h1", {"itemprop": "name"}).text
         seller = soup.find("div", {"class": "card-body"}).find("a").text.strip()
@@ -86,57 +84,94 @@ class Product:
         price = soup.find("meta", {"itemprop": "price"}).get("content")
         price = int(price.split(".")[0])
 
-        ram_tuple = Product.ram_pattern.findall(
-            soup.find("div", {"class": "excerpt"}).text.strip()
+        ram = Product._ram_pattern.findall(soup.find("div", {"class": "excerpt"}).text)[0]
+
+        memory = " ".join(
+            Product._rom_pattern.findall(soup.find("div", {"class": "excerpt"}).text)[0]
         )
 
-        if ram_tuple is not None and len(ram_tuple) > 0:
-            ram = "".join(ram_tuple[0])
-        else:
-            ram = None
-
-        memory_tuple = Product.rom_pattern.findall(
-            soup.find("div", {"class": "excerpt"}).text.strip()
-        )
-
-        if memory_tuple is not None and len(memory_tuple) > 0:
-            memory = " ".join(memory_tuple[0])
-        else:
-            memory = None
+        # ram and memory sometimes throw "out of range" error 
+        # for phones that have no data about ram and memory
 
         return Product(title=title, price=price, seller=seller, ram=ram, memory=memory)
+
+
+class Parser:
+    """
+    alfa.kz website parser
+    """
+
+    PARSER = "html.parser"
+
+    SCHEMA = "https"  # not in __init__ because it's not likely to be redefined
+    HOST = "alfa.kz"
+
+    PHONES_PATH = "phones/telefony-i-smartfony"  
+    # for this task it can be hardcoded, for production version it should be an element of category tree
+
+    def __init__(self, parser: str = PARSER):
+        self.parser = parser
+
+    @log
+    def get_products(self, page: int) -> Union[Product, Exception]:
+        """
+        yields instances of Product class from page
+        """
+
+        url = (
+            f"{Parser.SCHEMA}://{Parser.HOST}/{Parser.PHONES_PATH}/page{page}#products"
+        )
+
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            raise requests.exceptions.HTTPError(
+                f"status_code = {response.status_code}, url = {url}"
+            )
+
+        soup = BeautifulSoup(  
+            response.text, features=Parser.PARSER
+        ) 
+        # regex parser would probably increase performance, BS decreases dev time
+
+        products = soup.find_all("div", {"itemtype": Product._itemtype})
+
+        links = [
+            product.find("div", {"class": "title"}).find("a").get("href")
+            for product in products
+        ]
+
+        for link in links:
+            try:
+                yield Product.from_link(link)
+            except Exception as e:
+                yield e
 
 
 if __name__ == "__main__":
 
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("-n", dest="n", type=int, required=True)
-    args = arg_parser.parse_args()
+    cli_args = arg_parser.parse_args()
 
     parser = Parser()
 
-    links = []
-
-    for i in range(args.n):
-        links += parser.get_products_from_page(i + 1)
+    logger.setLevel(logging.WARNING)
 
     products = []
 
-    for link in links:
-        try:
-            products.append(
-                Product.from_link(link).__dict__
-            )  # due to bug occured at https://alfa.kz/phones/telefony-i-smartfony/xiaomi/redmi_note_11_pro_8_128gb/7643584
-        except Exception as e:
-            print(f"Could not parse {link}, skipped. Error: {e}")
+    for page in range(cli_args.n):
 
-    output_path = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)),
-        f"data/output_{int(time.time())}.csv",
-    )
+        products += [
+            product.__dict__
+            for product in parser.get_products(page + 1)
+            if product is not None
+        ]
 
-    with open(output_path, "w", newline="\n") as f:
-        title = [field for field in Product.__dataclass_fields__]
+    with open(OUTPUT_PATH, "w", newline="\n") as f:
+
+        title = list(Product.__dataclass_fields__)
+
         writer = csv.DictWriter(
             f, title, delimiter=",", quotechar='"', quoting=csv.QUOTE_NONNUMERIC
         )
